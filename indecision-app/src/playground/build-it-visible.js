59bbfbe13eb80610c0c1010e684bd1afe4977abf
Giedre Klimaite <giedre.klima@gmail.com>
2021-03-30 15:09:17 +0300

    stete and props

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,25 +1,59 @@
-let see = false;
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false,
+    };
+  }
 
-const toggleText = () => {
-  see = !see;
-  render();
-};
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility,
+      };
+    });
+  }
 
-const render = () => {
-  const jsx = (
-    <div>
-      <h1>Title</h1>
-      <button onClick={toggleText}>
-        {see ? "Hide details" : "Show details"}
-      </button>
-      {see && (
-        <div>
-          <p>Hey. These are some details you can see!</p>
-        </div>
-      )}
-    </div>
-  );
-  ReactDOM.render(jsx, document.getElementById("app"));
-};
+  render() {
+    return (
+      <div>
+        <h1>Title</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? "Hide details" : "Show details"}
+        </button>
 
-render();
+        {this.state.visibility && (
+          <div>
+            <p>Hey. These are some details you can see!</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"));
+// let see = false;
+// const toggleText = () => {
+//   see = !see;
+//   render();
+// };
+// const render = () => {
+//   const jsx = (
+//     <div>
+//       <h1>Title</h1>
+//       <button onClick={toggleText}>
+//         {see ? "Hide details" : "Show details"}
+//       </button>
+//       {see && (
+//         <div>
+//           <p>Hey. These are some details you can see!</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+//   ReactDOM.render(jsx, document.getElementById("app"));
+// };
+
+// render();
